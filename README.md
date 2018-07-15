795b5ea1c5c9733b2b2f3880879704264832ba15
Dijker <Geurt@Dijker.nu>
2018-07-15 16:01:43 +0200

    Initial code
    Initial code

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Eliza
 
 Eliza is a mock Rogerian psychotherapist. Original program by Joseph Weizenbaum.
+
+ELIZA is a natural language conversation program described by Joseph Weizenbaum in January 1966. It features the dialog between a human user and a computer program representing a mock Rogerian psychotherapist.
+The original program was implemented on the IBM 7094 of the Project MAC time-sharing system at MIT and was written in MAD-SLIP. For more information visit https://en.wikipedia.org/wiki/ELIZA
+
+For a conversation example given by Joseph Weizenbaum in his article see the Eliza Test page. (http://www.masswerk.at/elizabot/eliza_test.html)
+
+This is an implementation based on the promised based node.js module https://github.com/natelewis/eliza-as-promised.
+
+All credits for Eliza go to Joseph Weizenbaum for Eliza,
+eliza-as-promised module by Nate Lewis.
+Homey App by Geurt Dijker.
+
+This app is For Educational Purposes Only.
+
+Talk to a real person?
+You can also get help with talking about your feelings with real persons,
+ask the people around you or chat with a Christian who cares:
+https://chatnow.org
+
+Dutch:
+Met een echt persoon praten?
+Voor begeleiding bij het verwerken van je gevoelens kun je ook bij veel  hulpinstanties en eventueel online terrecht:
+https://nl.wikipedia.org/wiki/Korrelatie
+https://mindkorrelatie.nl//
--- app.js
+++ app.js
@@ -1,13 +1,68 @@
 'use strict';
 
 const Homey = require('homey');
+const Eliza = require( 'eliza-as-promised');
+var eliza = new Eliza();
+var speechSession = null;
+var responseU = null;
+var responseE = null;
 
-class MyApp extends Homey.App {
-	
+class ElizaApp extends Homey.App {
+	queryUser ( responseE ) {
+		responseU  = speechSession.ask( responseE , {session: speechSession, timeout: 30000})
+			.then (( responseU ) => {
+				if ( responseU.length > 0 ) {
+					this.log('L >> ' + responseU.length );
+					this.log('User >> ' + responseU );
+					responseE = eliza.getResponse(responseU)
+					// console.log('>>re ' + responseE.reply);
+						.then((responseE) => {
+							if (responseE.reply) {
+								this.log('Eliza >> ' + responseE.reply);
+								this.queryUser( responseE.reply )
+							}
+							if (responseE.final) {
+								this.log('Final Response from Eliza >> ' + responseE.final );
+								speechSession.say( responseE.final );
+								// process.exit(0);
+							}
+					})
+					.catch((err) => {
+						//reject(err);
+						this.log( '>> No Response from Eliza !!' );
+					})
+			}
+		})
+		.catch((err) => {
+			// reject(err);
+			this.log( '>> Ask timed out !!' );
+		})
+	};
+
+	/**
+	 * Initializes the application.
+	 */
 	onInit() {
-		this.log('MyApp is running...');
+		this.log('Eliza is running...');
+		this.log('Can I Speak Eliza?');
+		this.log('I feel  lonely.');
+		Homey.ManagerSpeechInput.on('speechEval', (speech, callback) => {
+				callback(null, true);
+			});
+		Homey.ManagerSpeechInput.on('speechMatch', (speech, onSpeechEvalData) => {
+			this.log('Eliza speechMatch ...');
+			//var elizan = new Elizan();
+			// start an Eliza conversation
+			responseU =  null;
+			responseE =  eliza.getInitial();
+			this.log('New Session >> ' + responseE );
+			speechSession = speech;
+			this.queryUser( responseE );
+			// console.log('<<< ' + responseU );
+		})
 	}
-	
 }
 
-module.exports = MyApp;
+module.exports = ElizaApp;
+// Can I Speak Eliza?
+// I am lonely
